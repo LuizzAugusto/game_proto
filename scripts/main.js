ae7917022ee90200df599b7ef5695136ec477c46
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2024-01-06 19:43:50 -0300

    add player

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,35 @@ if (canvas?.tagName == "CANVAS") {
     canvas.width = 500;
     canvas.height = 500;
     canvas.style.border = "1px solid #ccc";
+    const player = createSprite("green", 0, 0, 50, 50);
+    drawSprite(player, ctx);
   }
   else
     alert("error, can't get canvas context");
+}
+
+/**
+ * 
+ * @param {string} color 
+ * @param {number} x 
+ * @param {number} y 
+ * @param {number} width 
+ * @param {number} height 
+ * @param {boolean|undefined} visible 
+ * @returns {import("./types").SpriteType}
+ */
+function createSprite(color, x, y, width, height, visible = true) {
+  return { color, x, y, width, height, visible };
+}
+
+/**
+ * 
+ * @param {import("./types").SpriteType} sprite 
+ * @param {CanvasRenderingContext2D} ctx 
+ */
+function drawSprite({ color, x, y, width, height, visible }, ctx) {
+  if (visible) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, width, height);
+  }
 }
---
+++ scripts/types.d.ts
@@ -0,0 +1,2 @@
+export type DimensionType = { x: number, y: number, width: number, height: number };
+export type SpriteType = DimensionType & { color: string, visible: boolean };
