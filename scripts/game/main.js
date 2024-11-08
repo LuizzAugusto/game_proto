08b5c53f9d7d16639e05722a93da0b9184ace328
Luiz Augusto Schneider da Silva <luizz.augustoll@hotmail.com>
2024-11-08 19:17:17 -0300

    add TextState Type

--- scripts/game/main.js
+++ scripts/game/main.js
@@ -6,6 +6,13 @@ import { createDrawSubject } from "./view.js";
 
 /**
  * 
+ * @typedef {Object} TextState
+ * @property {number} score
+ * @property {number} timeLeft
+ */
+
+/**
+ * 
  * @param {CanvasRenderingContext2D} ctx 
  */
 export function createGame(ctx) {
@@ -13,6 +20,10 @@ export function createGame(ctx) {
     const player = createPlayer(canvasDimension);
     const sprites = [createTarget(canvasDimension), createTarget(canvasDimension), createTarget(canvasDimension), createTarget(canvasDimension), player];
     const playerSpeed = 10;
+    /**
+     * 
+     * @type {TextState}
+     */
     const textState = { score: 0, timeLeft: 3 };
 
     bindPlayerControlToKeyboard(player, playerSpeed);
@@ -26,7 +37,7 @@ export function createGame(ctx) {
 
 /**
  * 
- * @param {[sprites: import("./utils/spriteUtils.js").SpriteType[], canvasDimension: import("./utils/spriteUtils.js").DimensionType, {score: number, timeLeft: number}]} state 
+ * @param {[sprites: import("./utils/spriteUtils.js").SpriteType[], canvasDimension: import("./utils/spriteUtils.js").DimensionType, TextState]} state 
  * @param {import("../utils/ObservableSubject.js").ObservableSubjectType[]} subjects 
  */
 function update(state, subjects) {
